630c76b83e3724087e5ee6a09821a79b48d74577
mounir <mounirghafirlw@gmail.com>
2026-02-25 15:29:10 +0000

    i add the delete and update

--- Controllers/walletController.js
+++ Controllers/walletController.js
@@ -29,4 +29,31 @@ const createWallet = (req, res) => {
   res.status(201).json(newWallet);
 };
 
-module.exports = { getAllWallets, getWalletById, createWallet };
+
+const deleteWallet = (req, res) => {
+  const index = wallets.findIndex(w => w.id === parseInt(req.params.id));
+
+  if (index === -1) {
+    return res.status(404).json({ message: 'Wallet not found' });
+  }
+
+  wallets.splice(index, 1);
+
+  res.json({ message: 'Wallet deleted successfully' });
+};
+
+const updateWallet = (req, res) => {
+  const wallet = wallets.find(w => w.id === Number(req.params.id));
+
+  if (!wallet) {
+    return res.status(404).json({ message: 'Wallet not found' });
+  }
+
+  const { name } = req.body;
+
+  wallet.name = name;
+
+  res.json({ message: 'Wallet updated successfully', wallet });
+};
+
+module.exports = { getAllWallets, getWalletById, createWallet, deleteWallet, updateWallet };
--- Routers/walletRouter.js
+++ Routers/walletRouter.js
@@ -6,5 +6,7 @@ const validateWallet = require('../Middlewares/walletMiddleware');
 router.get('/', walletController.getAllWallets);
 router.get('/:id', walletController.getWalletById);
 router.post('/', validateWallet, walletController.createWallet);
+router.delete('/:id', walletController.deleteWallet);
+router.put('/:id', validateWallet, walletController.updateWallet);
 
 module.exports = router;
